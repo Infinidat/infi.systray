0a84baff0916e8469bd8f21d2999f3f8be53c08b
Freddie Meyer <dungfu3+github@gmail.com>
2018-04-22 21:58:05 -0700

    Disable import of pkg_resources for Python 3.3+

--- src/infi/systray/__init__.py
+++ src/infi/systray/__init__.py
@@ -1,2 +1,4 @@
-__import__("pkg_resources").declare_namespace(__name__)
+import sys
+if hex(sys.hexversion) < '0x30300f0':
+    __import__("pkg_resources").declare_namespace(__name__)
 from .traybar import SysTrayIcon
